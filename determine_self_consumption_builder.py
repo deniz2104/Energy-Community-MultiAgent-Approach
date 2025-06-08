daf8c50398ea896f63d44eee7c54a4c2071928ef
deniz2104 <sined2101@gmail.com>
2025-06-08 23:38:14 +0300

    removed redudant line; it was happening

--- determine_self_consumption_builder.py
+++ determine_self_consumption_builder.py
@@ -5,7 +5,6 @@ class SelfConsumptionBuilder:
     def build_self_consumption(self,houses):
         self_consumption_house ={}
         for house in houses:
-            if(house.house_id not in self_consumption_house):
-                self_consumption_house[house.house_id] = SelfConsumption(house.house_id)
-                self_consumption_house[house.house_id].determine_self_consumption_over_time()
+            self_consumption_house[house.house_id] = SelfConsumption(house.house_id)
+            self_consumption_house[house.house_id].determine_self_consumption_over_time()
         return list(self_consumption_house.values())
