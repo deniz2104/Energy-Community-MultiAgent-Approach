b14387f19c83807ae06cd51beddd4157d77a6804
deniz2104 <sined2101@gmail.com>
2025-06-09 12:15:43 +0300

    add make_recommendation method in manager based on weekly datas and suggest recommendation

--- ManagerAgent.py
+++ ManagerAgent.py
@@ -1,22 +1,35 @@
 from mesa import Agent
-
+from HouseAgent import HouseAgent
 class ManagerAgent(Agent):
     def __init__(self,unique_id,model):
         super.__init__(unique_id,model)
 
-        self.current_recomandation = None
+        self.current_recommendation = None
         self.recommendation_history = []
         self.feedback_history = []
 
     def make_recommendation(self):
-        pass
+        current_step=self.model.step_count
+        current_week= current_step // 168
+
+        houses = [agent for agent in self.model.schedule.agents if isinstance(agent, HouseAgent)]
+        for house in houses:
+            current_consumption = house.base_consumption[current_step]
+            weekly_avg = house.weekly_consumption[current_week]
+
+            if current_consumption >1.1*weekly_avg:
+                house.current_recommendation = "increase"
+            elif current_consumption <0.9*weekly_avg:
+                house.current_recommendation = "decrease"
+            else:
+                house.current_recommendation = "maintain"
 
     def receive_feedback(self,follow_recommendation):
         self.feedback_history.append(1 if follow_recommendation else 0)
     
     def step(self):
         recommendation = self.make_recommendation()
-        self.current_recomandation=recommendation
+        self.current_recommendation=recommendation
         self.recommendation_history.append(recommendation)
 
         if self.model.step_count > 0:
