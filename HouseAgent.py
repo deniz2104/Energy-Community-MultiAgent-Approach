a72cea71118f83ebc22fade8d9ca5baa321487d7
deniz2104 <sined2101@gmail.com>
2025-06-09 10:40:42 +0300

    add method for retrieving weekly data;useful in my recommendation system; commented method convert_time_to_steps it might be redundant

--- HouseAgent.py
+++ HouseAgent.py
@@ -1,15 +1,33 @@
 from mesa import Agent
+from statistics import mean
 class HouseAgent(Agent):
     def __init__(self,unique_id,model,house_obj,agent_type="normal"):
         super.__init__(unique_id,model)
         self.base_consumption = house_obj.consumption
+        self.weekly_consumption = self.define_weekly_consumption()
         self.agent_type = agent_type
-        self.current_consumption=0
+        self.current_consumption = 0
+        self.simulated_consumption = {}
         self.current_recommendation = None
         self.followed_recommendation = False
         self.last_action = "maintain"
 
         self.set_agent_type()
+        self.convert_timestamp_to_steps(house_obj)
+    
+    #def convert_timestamp_to_steps(self,house_obj):
+    #    values=list(house_obj.consumption.values())
+    #    list_of_numbers=list(range(1,len(house_obj.consumption)))
+    #    self.base_consumption = {str(num): value for num, value in zip(list_of_numbers, values)}
+
+    def define_weekly_consumption(self):
+        weekly_consumption = {}
+        for i in range(len(self.base_consumption)//168):
+            weekly_consumption[i] = mean(list(self.base_consumption.values())[i*168:(i+1)*168])
+        if((len(self.base_consumption)-len(weekly_consumption)*168) % 168!= 0):
+               weekly_consumption[len(weekly_consumption)] = mean(list(self.base_consumption.values())[-(len(self.base_consumption)-len(weekly_consumption)*168):])
+        return weekly_consumption
+
         
     def set_agent_type(self):
         if self.agent_type == "non-enthusiastic":
@@ -51,6 +69,7 @@ class HouseAgent(Agent):
         "strongly_decrease": 1 - delta_p_strong,
     }
         self.current_consumption = self.base_consumption * multipliers.get(action, 1.0)
+        self.simulated_consumption[self.model.step_count] = self.current_consumption
     def step(self):
         action=self.decide_action()
         self.last_action = action
