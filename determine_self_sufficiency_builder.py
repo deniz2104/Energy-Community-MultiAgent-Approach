99c57f2782b98434e5c778866dd3ce8f88ee7921
deniz2104 <sined2101@gmail.com>
2025-06-14 19:37:34 +0300

    modified the function, filled consumption and power_estimated fields used for self_sufficiency

--- determine_self_sufficiency_builder.py
+++ determine_self_sufficiency_builder.py
@@ -1,12 +1,15 @@
 from determine_self_sufficiency import SelfSufficiency
-
-class SelfSufficiencyBuilder:
+from determine_self_consumption_builder import SelfConsumptionBuilder
+class SelfSufficiencyBuilder(SelfConsumptionBuilder):
     def __init__(self):
         pass
 
-    def build_self_sufficiency(self, houses):
+    def build_self_sufficiency(self, houses,power_estimated_houses):
         self_sufficiency_house = {}
+        power_dict = {house.house_id: house for house in power_estimated_houses}
         for house in houses:
             self_sufficiency_house[house.house_id] = SelfSufficiency(house.house_id)
+            self_sufficiency_house[house.house_id].consumption = house.consumption
+            self_sufficiency_house[house.house_id].power_estimated = power_dict[house.house_id].power_estimated
             self_sufficiency_house[house.house_id].determine_self_sufficiency_over_time()
         return list(self_sufficiency_house.values())
