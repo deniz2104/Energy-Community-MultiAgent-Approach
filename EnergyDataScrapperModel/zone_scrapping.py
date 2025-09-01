d05bdd7d3fd2e320c8eeabd888e66f8795e6a683
deniz2104 <sined2101@gmail.com>
2025-09-01 15:50:00 +0300

    modified script, made it faster using defaultdict and no need for sorting

--- EnergyDataScrapperModel/zone_scrapping.py
+++ EnergyDataScrapperModel/zone_scrapping.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
 import requests
 
 url = "https://posf.ro/api/v1/comparator?request=get-judete"
 response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"})
 json_response = response.json()
-dictionary_of_zones_with_id :dict[str,list[str]] = {}
+dictionary_of_zones_with_id = defaultdict(list)
 for data in json_response:
-    dictionary_of_zones_with_id[data["id_zona"]] = dictionary_of_zones_with_id.get(data["id_zona"], []) + [data["nume"]]
-dictionary_of_zones_with_id = dict(sorted(dictionary_of_zones_with_id.items(), key=lambda x: int(x[0])))
+    zone_id = data["id_zona"]
+    zone_name = data["nume"]
+    dictionary_of_zones_with_id[zone_id].append(zone_name)
